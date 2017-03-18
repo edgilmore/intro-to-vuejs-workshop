e0e85b82aeca1b16bcd747d7ba80713762f1e203
Ed Gilmore <edgilmore@live.com>
2017-03-18 13:01:57 -0700

    added base component for todo app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <input type="text" v-model="todo" v-on="keyup: submitForm | key 'enter'"/><button type="button" @click="addTodo">Add</button>
+    <div>
+      <h2>Todos</h2>
+      <ul>
+        <li v-for="todo in todoList">
+          {{todo}}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -24,37 +17,28 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      todo: '',
+      todoList: []
     }
+  },
+  methods: {
+    addTodo: function(){
+      this.todoList.push(this.todo)
+    },
+    removeTodo: function(){
+      if(this.todoList.indexOf(this.todo) > -1)
+      {
+        var i
+        this.todoList[]
+      }
+    }
+  },
+  computed: function() {
+    
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
